8285c2f42c358e691ca2dc18b1b3c0d9fe7bc1dd
RM22065 <rm22065@ues.edu.sv>
2026-05-21 12:03:48 -0600

    Update profesor_service.rs

--- src/service/profesor_service.rs
+++ src/service/profesor_service.rs
@@ -9,3 +9,36 @@ pub async fn listar_profesores(
 
     profesor_repository::obtener_profesores(pool).await
 }
+
+pub async fn buscar_profesor(
+    pool: &PgPool,
+    id: i32,
+) -> Result<Profesor, sqlx::Error> {
+
+    profesor_repository::obtener_profesor_por_id(pool, id).await
+}
+
+pub async fn crear_profesor(
+    pool: &PgPool,
+    profesor: Profesor,
+) -> Result<(), sqlx::Error> {
+
+    profesor_repository::crear_profesor(pool, profesor).await
+}
+
+pub async fn actualizar_profesor(
+    pool: &PgPool,
+    id: i32,
+    profesor: Profesor,
+) -> Result<(), sqlx::Error> {
+
+    profesor_repository::actualizar_profesor(pool, id, profesor).await
+}
+
+pub async fn eliminar_profesor(
+    pool: &PgPool,
+    id: i32,
+) -> Result<(), sqlx::Error> {
+
+    profesor_repository::eliminar_profesor(pool, id).await
+}
