dfd2e4d288467208db53df45fe057cb61655f527
RM22065 <rm22065@ues.edu.sv>
2026-05-21 11:26:12 -0600

    Update profesor_repository.rs

--- src/repository/profesor_repository.rs
+++ src/repository/profesor_repository.rs
@@ -1,10 +1,16 @@
-use sqlx::PgPool;
+use sqlx::{PgPool, query_as};
+
 use crate::models::profesor::Profesor;
 
-pub async fn obtener_profesores(pool: &PgPool) -> Result<Vec<Profesor>, sqlx::Error> {
-    sqlx::query_as::<_, Profesor>(
+pub async fn obtener_profesores(
+    pool: &PgPool,
+) -> Result<Vec<Profesor>, sqlx::Error> {
+
+    let profesores = query_as::<_, Profesor>(
         "SELECT id, nombre, apellido, correo, especialidad FROM profesores"
     )
     .fetch_all(pool)
-    .await
+    .await?;
+
+    Ok(profesores)
 }
