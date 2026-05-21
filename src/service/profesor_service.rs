56c5a8728c14c02495e5616988a054d4fae540ac
RM22065 <rm22065@ues.edu.sv>
2026-05-21 11:27:13 -0600

    Refactor listar_profesores function signature

--- src/service/profesor_service.rs
+++ src/service/profesor_service.rs
@@ -3,8 +3,9 @@ use sqlx::PgPool;
 use crate::models::profesor::Profesor;
 use crate::repository::profesor_repository;
 
-pub async fn listar_profesores(pool: &PgPool)
--> Result<Vec<Profesor>, sqlx::Error> {
+pub async fn listar_profesores(
+    pool: &PgPool,
+) -> Result<Vec<Profesor>, sqlx::Error> {
 
     profesor_repository::obtener_profesores(pool).await
 }
