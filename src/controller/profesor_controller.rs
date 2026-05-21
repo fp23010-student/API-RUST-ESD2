b4a1f217726d28aae4884c517b1efde819386d7d
RM22065 <rm22065@ues.edu.sv>
2026-05-21 11:28:16 -0600

    Refactor get_profesores to use axum framework

--- src/controller/profesor_controller.rs
+++ src/controller/profesor_controller.rs
@@ -1,14 +1,19 @@
-use actix_web::{web, HttpResponse, Responder};
+use axum::{
+    extract::State,
+    Json,
+    http::StatusCode,
+};
+
 use sqlx::PgPool;
 
 use crate::service::profesor_service;
 
 pub async fn get_profesores(
-    pool: web::Data<PgPool>,
-) -> impl Responder {
+    State(pool): State<PgPool>,
+) -> Result<Json<serde_json::Value>, StatusCode> {
 
-    match profesor_service::listar_profesores(pool.get_ref()).await {
-        Ok(profesores) => HttpResponse::Ok().json(profesores),
-        Err(_) => HttpResponse::InternalServerError().finish(),
+    match profesor_service::listar_profesores(&pool).await {
+        Ok(profesores) => Ok(Json(serde_json::json!(profesores))),
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
     }
 }
