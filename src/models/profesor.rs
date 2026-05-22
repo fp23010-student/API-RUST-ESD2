f0d3dc845870b75ebb5f009692ca120bc8c94082
RM22065 <rm22065@ues.edu.sv>
2026-05-21 20:21:50 -0600

    Add CreateProfesor struct to profesor.rs
    Added CreateProfesor struct for creating new Profesor instances.

--- src/models/profesor.rs
+++ src/models/profesor.rs
@@ -1,9 +1,16 @@
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 
-#[derive(Debug, Serialize, Deserialize, FromRow)]
+#[derive(Serialize, Deserialize, FromRow, Debug)]
 pub struct Profesor {
     pub id: i32,
+    pub nombre: String,
+    pub correo: String,
+    pub especialidad: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CreateProfesor {
     pub nombre: String,
     pub correo: String,
     pub especialidad: String,
